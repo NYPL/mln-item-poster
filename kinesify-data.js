3bef3ef4b18b45ec056ea7277f0698d07479cab5
Brian Foo <brian@youaremyjoy.org>
2017-03-09 15:16:22 -0500

    update kinesify data script to pull schema from api

--- kinesify-data.js
+++ kinesify-data.js
@@ -1,39 +1,45 @@
 // Usage:
 //  The following command will take unencoded json, encode it with avro schema, encode it with base64, and put in Kinesis format.
-//    node kinesify-data.js event.unencoded.json event.json
+//    node kinesify-data.js event.unencoded.bibs.json event.json https://api.nypltech.org/api/v0.1/current-schemas/Bib
 
 const args = process.argv.slice(2);
 const avro = require('avsc');
 const config = require('config');
 const fs = require('fs');
+const request = require('request');
 const _ = require('highland');
 
 // config
 const infile = args[0];
 const outfile = args[1];
+const schemaUrl = args[2];
 
-// initialize avro schema
-const avroType = avro.parse(config.get('schema'));
+function onSchemaLoad(schema){
+  // initialize avro schema
+  var avroType = avro.parse(schema);
 
-// read unencoded data
-var unencodedData = JSON.parse(fs.readFileSync(infile, 'utf8'));
+  // read unencoded data
+  var unencodedData = JSON.parse(fs.readFileSync(infile, 'utf8'));
 
-// encode data and put in kinesis format
-var kinesisEncodedData = unencodedData.Records
-  .map(kinesify);
+  // encode data and put in kinesis format
+  var kinesisEncodedData = unencodedData.Records
+    .map(function(record){
+      kinesify(record, avroType);
+    });
 
-// stringify and write to file
-var json = JSON.stringify({"Records": kinesisEncodedData});
-fs.writeFile(outfile, json, 'utf8', function(err, data){
-  if (err) {
-    console.log('Write error:', err);
+  // stringify and write to file
+  var json = JSON.stringify({"Records": kinesisEncodedData});
+  fs.writeFile(outfile, json, 'utf8', function(err, data){
+    if (err) {
+      console.log('Write error:', err);
 
-  } else {
-    console.log('Successfully wrote data to file');
-  }
-});
+    } else {
+      console.log('Successfully wrote data to file');
+    }
+  });
+}
 
-function kinesify(record){
+function kinesify(record, avroType){
   // encode avro
   var buf = avroType.toBuffer(record);
   // encode base64
@@ -56,3 +62,15 @@ function kinesify(record){
     "eventSourceARN": "arn:aws:kinesis:EXAMPLE"
   };
 }
+
+var options = {
+  uri: schemaUrl,
+  json: true
+};
+request(options, function(error, resp, body){
+  if (body.data && body.data.schema) {
+    console.log('Loaded schema');
+    var schema = JSON.parse(body.data.schema);
+    onSchemaLoad(schema);
+  }
+});
